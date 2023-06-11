9dee26c655a90363b1af9474e5d575f216d14aa2
370025263 <370025263@qq.com>
2023-06-11 22:24:43 +0800

    output BUG

--- route/main.py
+++ route/main.py
@@ -1,20 +1,20 @@
 from route import AstarSolver,RRTSolver,BiRRTSolver
 from map import Map
+if __name__ == "__main__":
+    # generate maze from image
+    map = Map("de_dust2")
 
-# generate maze from image
-map = Map("de_dust2")
-
-# solve maze via solver
-#solver = AstarSolver(map_=map)
-#solution = solver.solve_maze(start=(178,57),end=(31, 159),radius=80,scale=1)
-# solver = RRTSolver(map_=map)
-# solution = solver.solve_maze(start=(178,57),end=(31, 159),radius=3,iter=2000,step_num=5,step_len=2)
-solver = BiRRTSolver(map_=map)
-solution = solver.solve_maze(start=(178,57),end=(31, 159),radius=2,iter=5000,step_num=2,step_len=1,prob=0.9,draw=1)
+    # solve maze via solver
+    #solver = AstarSolver(map_=map)
+    #solution = solver.solve_maze(start=(178,57),end=(31, 159),radius=80,scale=1)
+    # solver = RRTSolver(map_=map)
+    # solution = solver.solve_maze(start=(178,57),end=(31, 159),radius=3,iter=2000,step_num=5,step_len=2)
+    solver = BiRRTSolver(map_=map)
+    solution = solver.solve_maze(start=(178,57),end=(31, 159),radius=2,iter=5000,step_num=2,step_len=1,prob=0.9,draw=1)
 
 
 
-# visualize solution
-solver.draw_routes(solution, map.bit_map, True)
+    # visualize solution
+    solver.draw_routes(solution, map.bit_map, True)
 
-print(solution)
+    print(solution)
